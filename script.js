90c4ad9ed277a1a6a5f05190a68f99cd73b2898c
yusuf <yusufkhalilahmed@gmail.com>
2022-06-27 23:55:30 +0100

    style change

--- script.js
+++ script.js
@@ -17,13 +17,20 @@ async function fetchObject(url, payload) { //this returns a javascript object
 async function coinmap(){
     let users = await fetchObject("https://randomuser.me/api/")
 
+    let w=document.createElement("h2")
+    w.innerText=users.results[0].name.title
+    document.body.appendChild(w)
+
     let location=document.createElement("h1")
     location.innerText=users.results[0].name.first
     document.body.appendChild(location)
 
-    let version=document.createElement("h2")
-    version.innerText=users.title
+
+    let version=document.createElement("h3")
+    version.innerText=users.results[0].name.last
     document.body.appendChild(version)
+
+
 
 }
 
